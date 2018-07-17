5e77e00f23c379e5c736a0c565d6ef01dce10607
Rafnazahg <devm.class@gmail.comdevm.class@gmail.comdem.class@gmail.com>
2018-07-17 16:04:48 +0300

    Initial Commit

---
+++ Java Challenges/src/Welcome.java
@@ -0,0 +1,13 @@
+/**
+ * Welcome to Java!
+ * Link: https://www.hackerrank.com/challenges/welcome-to-java/problem
+ */
+
+public class Welcome {
+
+    public static void main(String[] args) {
+        // simple print method
+        System.out.println("Hello, World.");
+        System.out.println("Hello, Java.");
+    }
+}
